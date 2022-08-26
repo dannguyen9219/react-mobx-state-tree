23c50ff814f167dfdce793ce6a0f652f34e03047
Dan Nguyen <nguyen.danimal@gmail.com>
2022-08-26 12:00:47 -0500

    add counter buttons

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,13 +1,23 @@
 import { observer } from "mobx-react"
 import { MSTStore } from "../models/root/root"
+import { Button } from "./button"
 
 export const Counter = observer(() => {
     const { counter } = MSTStore()
 
     return (
-        <div>
-            <h4>Counter</h4>
-            <p>{counter.count}</p>
+        <div className="flex flex-col items-center mt-20">
+            <h4 className="text-2xl font-bold text-center">Counter</h4>
+            <p
+              style={{ fontVariant: "tabular-nums" }}
+              className="text-2xl font-bold text-center"
+            >
+              {counter.count}
+            </p>
+            <div className="flex mt-2 space-x-3">
+              <Button label="-" onClick={counter.decrement} />
+              <Button label="+" onClick={counter.increment} />
+            </div>
         </div>
     )
 })
